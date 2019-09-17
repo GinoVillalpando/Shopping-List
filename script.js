f0739ca1858449b23750a432b06ee62621a7dfab
Gino Villalpando <grieveous@live.com>
2019-09-17 16:06:41 -0600

    part I: learn how to create a shopping list and the item values part II: learn how to input a delete button and it's values + properties"
    part II: how to add a delete button and it's values and properties

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ function initShoppingList() {
 	});
 }
 
-function handleItemForm(event, formRef) {
+function handleItemForm(event) {
 	if(event.preventDefault) {
 		event.preventDefault();
 	}
 
 	addItemToShoppingList();
-	formRef.reset();
 
 	return false;
 }
@@ -24,36 +23,15 @@ function handleItemForm(event, formRef) {
 function addItemToShoppingList() {
 	let itemName = document.getElementById("item-name");
 	let itemAmount = document.getElementById("item-amount");
-	let id = getRandomInt(0, 10000000);
 
-	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
 	console.log("Item HTML: ", itemHtml);
 	let itemListRef = document.getElementById("shopping-list");
 	itemListRef.insertAdjacentHTML("afterend", itemHtml);
-
-	setDeleteButtonEvent(id);
-}
-function setDeleteButtonEvent(id) {
-	let deleteButton = document.getElementById("button"+id);
-	deleteButton.addEventListener("click", () => {
-		removeListItem(id);
-	});
 }
 
-function createListItemHtml(itemName, itemAmount, id) {
-	return `<li id="item${id}">
+function createListItemHtml(itemName, itemAmount) {
+	return `<li>
 					${itemName} - ${itemAmount}
-					<button id="button${id}" type="button">Delete Item</button>
 			</li>`;
-}
-
-function removeListItem(id) {
-	let listItem = document.getElementById("item"+id);
-	listItem.parentNode.removeChild(listItem);
-}
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max-min)) + min;
 }
